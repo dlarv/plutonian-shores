aee2b13548ddbe31075c72043e24273b7f69cf5a
dlarv <hyrum21344@gmail.com>
2024-05-23 19:49:20 -0400

    Moved selection functionality into pt_core, so other utils can use it

--- cocytus/src/main.rs
+++ cocytus/src/main.rs
@@ -12,7 +12,7 @@
 
 use duct::cmd;
 use mythos_core::{cli::{clean_cli_args, get_cli_input}, logger::{get_logger_id, set_logger_id}, printinfo, printwarn};
-use pt_core::{Query, QueryError, QueryResult};
+use pt_core::{get_user_selection, validate_pkgs, Query, QueryError, QueryResult};
 fn main() {
     set_logger_id("COCYTUS");
     let args = clean_cli_args();
@@ -47,84 +47,6 @@ fn main() {
     };
 }
 
-fn validate_pkgs(search_terms: Vec<&str>) -> Option<Vec<QueryResult>> {
-    /*!
-     * Iterate over pkgs, searching for each one in repo. 
-     * Allows user to select from results or remove it.
-     * User also has opportunity to exit.
-     * Returns None if all packages are removed or user exits.
-     */
-    let mut output: Vec<QueryResult> = Vec::new();
-
-    for term in search_terms {
-        let query = match Query::query(&term) {
-            Ok(res) => res,
-            Err(QueryError::NotFound(msg)) | Err(QueryError::TertiaryList(msg)) => {
-                printwarn!("{msg}");
-                continue;
-            }
-        };
-        // No results, exit early.
-        if query.len() == 0 {
-            printwarn!("No results found for {term}");
-            continue;
-        }
-
-        let msg = &format!("{}\n0. Exit\n1. Select from result(s)\n2. Remove query\nOption: ", query.get_short_list());
-
-        // Get and validate user selection.
-        let user_input = get_user_selection(msg, 2);
-        if user_input == 0 {
-            return None;
-        }
-        if user_input == 2 {
-            printinfo!("Removed {term}");
-            continue;
-        }
-
-
-        // User chose to select from query results.
-        // If only one pkg exists, use it.
-        if query.len() == 1 {
-            output.push(query.get(0).unwrap().clone());
-            continue;
-        }
-        // Display results
-        let msg = query.get_short_list();
-        let selected_pkg_index = get_user_selection(&format!("{msg}\n0. Remove package\nEnter from the options above: "), query.len());
-
-        // User chose to remove package.
-        if selected_pkg_index == 0 {
-            printinfo!("Removed {term}");
-            continue;
-        }
-        // Add pkg
-        println!("Selected: {selected_pkg_index}");
-        output.push(query.get(selected_pkg_index - 1).unwrap().clone());
-    }
-
-    return Some(output);
-}
-fn get_user_selection(msg: &str, max_val: usize) -> usize {
-    /*!
-     * Prints a msg to the console prompting the user for input.
-     * Validates input. Input must be an integer [0,max_val].
-     * Returns that value
-     */ 
-    loop {
-        let input = match get_cli_input(msg).parse::<usize>() {
-            Ok(input) => input,
-            Err(_) => {
-                printwarn!("Please enter a valid number from the options above");
-                continue;
-            }
-        };
-        if input <= max_val {
-            return input;
-        }
-        printwarn!("Please enter a valid number from the options above");
-    }
-}
 
 fn print_pkg_info(pkgs: Query) {
     for pkg in pkgs {
--- pt_core/src/lib.rs
+++ pt_core/src/lib.rs
@@ -3,7 +3,7 @@ pub mod query;
 pub mod query_result;
 
 use duct::{Expression, cmd};
-use mythos_core::{printfatal, logger::get_logger_id};
+use mythos_core::{cli::get_cli_input, logger::get_logger_id, printfatal, printinfo, printwarn};
 use serde_derive::{Deserialize, Serialize};
 
 use std::io::{stdout, Write, stdin};
@@ -48,7 +48,85 @@ pub fn xbps_args_to_string(xbps_args: &Vec<String>) -> String {
         acc + x.trim_start_matches("-")
     });
 }
+pub fn validate_pkgs(search_terms: Vec<&str>) -> Option<Vec<QueryResult>> {
+    /*!
+     * Iterate over pkgs, searching for each one in repo. 
+     * Allows user to select from results or remove it.
+     * User also has opportunity to exit.
+     * Returns None if all packages are removed or user exits.
+     */
+    let mut output: Vec<QueryResult> = Vec::new();
 
+    for term in search_terms {
+        let query = match Query::query(&term) {
+            Ok(res) => res,
+            Err(QueryError::NotFound(msg)) | Err(QueryError::TertiaryList(msg)) => {
+                printwarn!("{msg}");
+                continue;
+            }
+        };
+        // No results, exit early.
+        if query.len() == 0 {
+            printwarn!("No results found for {term}");
+            continue;
+        } 
+        // User chose to select from query results.
+        // If only one pkg exists, use it.
+        if query.len() == 1 {
+            output.push(query.get(0).unwrap().clone());
+            continue;
+        }
+
+        let msg = &format!("{}\n\n0. Exit\n1. Select from result(s)\n2. Remove query\nOption: ", query.get_short_list());
+
+        // Get and validate user selection.
+        let user_input = get_user_selection(msg, 2);
+        if user_input == 0 {
+            return None;
+        }
+        if user_input == 2 {
+            printinfo!("Removed {term}");
+            continue;
+        }
+
+
+        // Display results
+        let msg = query.get_short_list();
+        let selected_pkg_index = get_user_selection(&format!("{msg}\n0. Remove package\nEnter from the options above: "), query.len());
+
+        // User chose to remove package.
+        if selected_pkg_index == 0 {
+            printinfo!("Removed {term}");
+            continue;
+        }
+        // Add pkg
+        println!("Selected: {selected_pkg_index}");
+        output.push(query.get(selected_pkg_index - 1).unwrap().clone());
+    }
+
+    return Some(output);
+}
+
+pub fn get_user_selection(msg: &str, max_val: usize) -> usize {
+    /*!
+     * Prints a msg to the console prompting the user for input.
+     * Validates input. Input must be an integer [0,max_val].
+     * Returns that value
+     */ 
+    loop {
+        let input = match get_cli_input(msg).parse::<usize>() {
+            Ok(input) => input,
+            Err(_) => {
+                printwarn!("Please enter a valid number from the options above");
+                continue;
+            }
+        };
+        if input <= max_val {
+            return input;
+        }
+        printwarn!("Please enter a valid number from the options above");
+    }
+}
 /* STRUCTS */
 /**
  * Public interface to results of query.
