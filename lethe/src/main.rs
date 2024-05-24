74fd5b3374786f8a82da3d82da9c2430d47fd263
dlarv <hyrum21344@gmail.com>
2024-05-24 16:07:14 -0400

    Created lethe basic functionality

--- lethe/src/main.rs
+++ lethe/src/main.rs
@@ -1,100 +1,72 @@
-pub mod remove_command;
-use pt_core::{help::print_help, query_manager};
-use mythos_core::{conf, logger::set_logger_id};
-use pt_core::{query_manager::QueryDisplayMode, MythosCommand};
-use remove_command::RemoveCommand;
+/*!
+ * Simple wrapper for xbps-remove command -Ro.
+ */
 
+use std::ffi::OsString;
+
+use duct::cmd;
+use mythos_core::{cli::{clean_cli_args, get_cli_input}, logger::{get_logger_id, set_logger_id}, printfatal, printinfo};
+use pt_core::{validate_pkgs, Query, QueryResult};
 fn main() {
-    set_logger_id("LETHE");
-    unsafe { 
-        if let Some(conf) = conf::MythosConfig::read_file("plutonian-shores") {
-            load_config_values(conf);
+    set_logger_id("COCYTUS");
+    let args = clean_cli_args();
+    let mut pkgs: Vec<&str> = Vec::new();
+    let mut opts: Vec<&str> = Vec::new();
+    let mut do_dry_run = false;
+
+    // Parse opts.
+    for arg in &args {
+        if arg == "-n" || arg == "--dryrun" {
+            do_dry_run = true;
+        }
+        else if arg.starts_with("-") {
+            opts.push(&arg);
+        } else {
+            pkgs.push(&arg);
         }
     }
-    let mut cmd = parse_args();
-    loop {
-        match cmd.execute() {
-            Ok(_) => break,
-            Err(_) => println!()
+
+    // Validate packages
+    // Ensure package(s) actually exist.
+    let mut removed_pkgs = false;
+    let validated_pkgs = Query::from(match validate_pkgs(pkgs) {
+        // Only grab packages that are installed.
+        Some(pkgs) => pkgs.into_iter().filter(|p| { removed_pkgs = true; p.is_installed}).collect::<Vec<QueryResult>>(),
+        None => {
+            printinfo!("Exiting");
+            return;
         }
+    });
+
+    // If all packages were removed, exit
+    if removed_pkgs {
+        println!("Removed packages not currently installed");
     }
+    if validated_pkgs.len() == 0 {
+        printinfo!("All packages were removed. Exiting...");
+        return;
+    }
+
+    // Give user option to exit.
+    let msg = format!("The following packages will be removed.{}\nWould you like to continue? Y/n ", validated_pkgs.get_short_list());
+    let user_input = get_cli_input(&msg).to_lowercase();
+    if user_input == "n" || user_input == "no" {
+        printinfo!("Exiting");
+        return;
+    }
+
+    // Create args list
+    let pkg_names = validated_pkgs.into_iter().map(|p| p.pkg_name.into()).collect::<Vec<String>>();
+    let mut args = vec!["-Ryo".to_string()];
+    if do_dry_run {
+        args.push("-n".into());
+    }
+    args.extend(pkg_names);
+
+    // Remove packages.
+    match cmd("xbps-remove", args).run() {
+        Ok(_) => printinfo!("Success! Exiting"),
+        Err(msg) => printfatal!("{msg}"),
+    }
+
 }
-unsafe fn load_config_values(conf: conf::MythosConfig) {
-    if let Some(conf) = conf.get_subsection("cocytus") { 
-        if let Some(val) = conf.try_get_float("fuzzy_find_threshold") {
-            query_manager::query_results::THRESHOLD = val as f32;
-        }
-
-        if let Some(val) = conf.try_get_integer("list_column_length") {
-            query_manager::query_results::LIST_COLUMN_LEN = val as usize;
-        }
-    }
-
-    if let Some(conf) = conf.get_subsection("lethe") {
-        if let Some(val) = conf.try_get_boolean("remove_orphans") {
-            remove_command::REMOVE_ORPHANS = val;
-        }
-        if let Some(val) = conf.try_get_boolean("do_recursive_removal") {
-            remove_command::DO_RECURSIVE = val;
-        }
-    }
-}
-
-fn parse_args() -> RemoveCommand {
-    let args = mythos_core::cli::clean_cli_args();
-    let mut cmd = RemoveCommand::new();
-    let mut reading_xbps_args = false;
-
-    for arg in args {
-        if arg.starts_with("-") {
-            if reading_xbps_args {
-                cmd.add_xbps_arg(arg);
-                continue;
-            }
-            match arg.as_str() {
-                "-h" | "--help" => {
-                    print_help();
-                    std::process::exit(0);
-                },
-                "-R" | "--recursive" => {
-                    cmd.set_do_recursive(true);
-                },
-                "-o" | "--remove-orphans" => {
-                    cmd.set_remove_orphans(true);
-                },
-                "-y" | "--assume-yes" => {
-                    cmd.set_assume_yes(true);
-                },
-                "-x" | "--xbps-args" => reading_xbps_args = true,
-                _ => { cmd.add_xbps_arg(arg); },
-            };
-        }
-        else {
-            cmd.add_pkg(arg);
-        }
-    }
-    return cmd;
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    pub fn parse_config() {
-        std::env::set_var("MYTHOS_LOCAL_CONFIG_DIR", "../../tests/config");
-        let conf = conf::MythosConfig::read_file("plutonian-shores").unwrap();
-
-        unsafe {
-            let prev_col_length = query_manager::query_results::LIST_COLUMN_LEN;
-            load_config_values(conf);
-            assert_eq!(query_manager::query_results::THRESHOLD, 1.0);
-            assert_eq!(query_manager::query_results::LIST_COLUMN_LEN, prev_col_length);
-            assert_eq!(remove_command::REMOVE_ORPHANS, true);
-            assert_eq!(remove_command::DO_RECURSIVE, false);
-        }
-
-    }
-}
-
