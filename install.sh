a16421d46609a706c79b73179127379f221a5cd6
dlarv <hyrum21344@gmail.com>
2024-11-24 01:48:36 -0500

    Moved charon to be its own thing.

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script intended to install charon, as it currently cannot install itself.
-
-cargo build --bin charon
-
-sudo cp "target/debug/charon" "/bin"
-sudo charon "charon/charon.charon"
-
